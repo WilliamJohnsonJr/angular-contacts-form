36361bab4806ac69d5d81b2233dda17abee87159
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-20 14:50:52 -0400

    Wrote submitForm() in controller so that on submit input values are injected into a comments section in the HTML. Styled in CSS

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,7 +1,25 @@
 function Controller ($scope) {
+	$scope.comments = [];	
 
-$scope.submitForm = function (){
+$scope.submitForm = function (name, email, website, message){
+		let newComment = {
+			id: "Comment " + ($scope.comments.length + 1),
+			name: name,
+			email: email,
+			website: website,
+			message: message
+		}
 
+		$scope.comments.push(newComment);
+
+		function printComment(){
+
+		}
+
+		$scope.name= '';
+		$scope.email='';
+		$scope.website='';
+		$scope.message='';
 }
 
 }
