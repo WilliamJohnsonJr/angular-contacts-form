3ff43bcb482fdb0e96242591fe3f7ead655deb26
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-20 15:52:52 -0400

    Wrote code in controller so that errors are thrown correctly on form submission

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -1,27 +1,58 @@
 function Controller ($scope) {
-	$scope.comments = [];	
+	$scope.comments = [];
+	$scope.errors = {
+		nameError: '',
+		emailError: '',
+		websiteError: '',
+		messageError: ''
+	}	
 
-$scope.submitForm = function (name, email, website, message){
-		let newComment = {
-			id: "Comment " + ($scope.comments.length + 1),
-			name: name,
-			email: email,
-			website: website,
-			message: message
-		}
+	$scope.submitForm = function (name, email, website, message){
+		if(name !== undefined){
+			if(email !== undefined && email.includes('@')){
+				if(website !== undefined && website.includes('http://')){
+					if(message !== undefined) {
+						let newComment = {
+							id: "Comment " + ($scope.comments.length + 1),
+							name: "Posted By: "+ name,
+							email: "Email: "+email,
+							website: "Website: "+website,
+							message: "Message: "+message
+						}
 
-		$scope.comments.push(newComment);
+						$scope.comments.push(newComment);
+						$scope.name= '';
+						$scope.email='';
+						$scope.website='';
+						$scope.message='';
+					} 
+				} 
+			} 
+		} 
+		if(name === undefined || name === "") {
+			$scope.errors.nameError = "Name cannot be left empty";
+		} else {
+			$scope.errors.nameError = "";
+		};
 
-		function printComment(){
+		if(email ===undefined || !email.includes('@')){
+			$scope.errors.emailError = "Email must contain an '@'";
+		} else {
+			$scope.errors.emailError = "";
+		};
 
-		}
+		if(website === undefined || website.substring(0,7) !=="http://") {
+			$scope.errors.websiteError = "Website must begin with 'http://'";
+		} else{
+			$scope.errors.websiteError = "";
+		};
 
-		$scope.name= '';
-		$scope.email='';
-		$scope.website='';
-		$scope.message='';
-}
-
+		if (message === undefined || message==="") {
+			$scope.errors.messageError = "Message cannot be left empty";
+		} else {
+			$scope.errors.messageError = "";
+		};
+	}
 }
 
 Controller.$inject = ['$scope'];
