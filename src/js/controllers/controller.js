68f75b8da1686d2c2fe4dd49f850da713106006d
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-20 23:42:59 -0400

    Edited server.js and controller.js so that communication to/from server allows for persistent data retrieval when form is submitted/comment is deleted/etc.

--- src/js/controllers/controller.js
+++ src/js/controllers/controller.js
@@ -5,8 +5,10 @@ function Controller ($scope, SERVER, $http) {
 	init();
 
 	function init () {
-    $http.get(SERVER.URL).then( (results) => {
-    		$scope.comments = results.data;
+    $http.get(SERVER.URL).then( (response) => {
+    		console.log(response.data);
+			$scope.comments = response.data;
+    		console.log($scope.comments);
     	});
   	}
 
@@ -20,17 +22,19 @@ function Controller ($scope, SERVER, $http) {
 	$scope.submitForm = function (name, email, website, message){
 		if(name !== undefined){
 			if(email !== undefined && email.includes('@')){
-				if(website !== undefined && website.substring(0,7) ==='http://' || website.substring(0,8)==="https://"){
+				if(website !== undefined && (website.substring(0,7) ==='http://' || website.substring(0,8)==="https://")){
 					if(message !== undefined) {
 						let newComment = {
-							id: "Comment " + ($scope.comments.length + 1),
+							commentNumber: "Comment " + ($scope.comments.length + 1),
 							name: "Posted By: "+ name,
 							email: "Email: "+email,
 							website: "Website: "+website,
 							message: "Message: "+message
 						}
-
-						$scope.comments.push(newComment);
+					    $http.post(SERVER.URL, newComment).then( (response) => {
+					    	newComment._id = response._id;
+					    	$scope.comments.push(response.data);
+					    });
 						$scope.name= '';
 						$scope.email='';
 						$scope.website='';
@@ -45,17 +49,25 @@ function Controller ($scope, SERVER, $http) {
 			$scope.errors.nameError = "";
 		};
 
-		if(email ===undefined || !email.includes('@')){
+		if(email ===undefined || email === ""){
+			$scope.errors.emailError = "Email cannot be left empty"
+		} else if (!email.includes('@')){
 			$scope.errors.emailError = "Email must contain an '@'";
 		} else {
 			$scope.errors.emailError = "";
 		};
 
-		if(website === undefined || website.substring(0,7) !=="http://" || website.substring(0,8)!=="https://") {
-			$scope.errors.websiteError = "Website must begin with 'http://' or 'https://'";
-		} else{
+		if(website === undefined || website===""){
+			$scope.errors.websiteError = "Website cannot be left empty";
+		} else if (website.substring(0,7) !=="http://") {
+			if (website.substring(0,8)!=="https://"){
+				$scope.errors.websiteError = "Website must begin with 'http://' or 'https://'";
+			} else{
 			$scope.errors.websiteError = "";
-		};
+			}
+		} else {
+			$scope.errors.websiteError = "";
+		}
 
 		if (message === undefined || message==="") {
 			$scope.errors.messageError = "Message cannot be left empty";
@@ -64,20 +76,16 @@ function Controller ($scope, SERVER, $http) {
 		};
 	}
 
-	$scope.addComment = (c) => {
-	    $http.post(SERVER.URL, c).then( (results) => {
-	    	$scope.comments.push(results.data);
-	    	$scope.c = {};
-	    });
-	}
-
 	$scope.deleteComment = (commentID) =>{
-		$http.delete(SERVER.URL + commentID).then( (results) => {
+		console.log(commentID);
+		$http.delete(SERVER.URL + commentID).then( (response) => {
 			$scope.comments = $scope.comments.filter((c)=>{
-				return c.id !== commentID;
+				return c._id !== commentID;
 			});
 		});
 	};	
+
+};
 
 Controller.$inject = ['$scope', 'SERVER', '$http'];
 export { Controller };
--- src/js/server.js
+++ src/js/server.js
@@ -1,5 +1,12 @@
 const SERVER = {
-	URL: "https://class-server.herokuapp.com/collections/monkey12346568432";
+	URL: "https://class-server.herokuapp.com/collections/candy1823874171/"
 }
 
+const OLDSERVER = {
+	URL: "https://class-server.herokuapp.com/collections/monkey12346568432/"
+}
+
+//note: there are two objects lost on the oldserver, because their ._id values were rewritten!
+
 export { SERVER };
+
